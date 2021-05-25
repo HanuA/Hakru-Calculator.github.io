53eef692f8d6e350acd13d2e43b4535bfbb694ee
Filip Hanuš <77624362+HanuA@users.noreply.github.com>
2021-05-25 16:05:30 +0200

    JS editted

--- js/HakruScript.js
+++ js/HakruScript.js
@@ -8,44 +8,77 @@ setTimeout(function(){
     result.value = "";
 }, 1500);
 let button = document.getElementsByClassName('btn');
-let lastAdded = 0;
+let lastNum = 0;
 for (let i = 0; i < button.length; i++){
     button[i].onclick = function(e) {
         let x = e.target;
+        let lastAdded = result.value.charAt(result.value.length-1);
+        console.log(lastNum)
         switch (x.innerHTML) {
             case "| x |":
-                result.value += (x.innerHTML + " ");
+                result.value += "|" + lastNum + "|";
+                lastNum = 0;
                 break;
-            case "sin()":
-                result.value += (x.innerHTML + " ");
-                break;
-            case "cos()":
-                result.value += (x.innerHTML + " ");
-                break;
-            case "tan()":
-                result.value += (x.innerHTML + " ");
-                break;
-            case "cotan()":
-                result.value += (x.innerHTML + " ");
+            case "sin(x)": case "cos(x)": case "tan(x)": case "cotan (x)":
+
+                lastNum = 0;
                 break;
             case "¹/ₓ":
-                result.value += (x.innerHTML + " ");
+
+                lastNum = 0;
                 break;
-            case "√":
-                result.value += (x.innerHTML + " ");
+            case "√(x)":
+                lastNum = 0;
                 break;
-            case "=":
-                result.value += (x.innerHTML + " ");
-                break;
+            case "=": case "^": case ".":
+                if (lastAdded == "" || lastAdded == "=" || lastAdded == "^" || lastAdded == "." ){
+                    break;
+                }
+                if (lastAdded == "=") {
+                    lastNum = 0;
+                }
+                else{
+                    result.value += (x.innerHTML);
+                    break;
+                }
+            case "+":
+            case "-":
+                if (lastAdded == "=" || lastAdded == "." ){
+                    break;
+                }
+                else{
+                    result.value += (x.innerHTML);
+                    break;
+                }
+            case "×":
+            case "÷":
+                if (lastAdded == "×" || lastAdded == "÷" || lastAdded == "^" || lastAdded == "." || lastAdded == "+" || lastAdded == "-"){
+                    break;
+                }
+                else{
+                    result.value += (x.innerHTML);
+                    lastNum = 0;
+                    break;
+                }
             case "C":
                 result.value = "";
+                lastNum = 0;
                 break;
-            case "":
+            case "": case '<i class="fas fa-backspace"></i>':
                 result.value = result.value.slice(0, result.value.length - 1);
                 break;
-                         
+            case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                if (lastNum == 0) {
+                    lastNum = (x.innerHTML);
+                }
+                else{
+                    lastNum += (x.innerHTML);
+                }
+                result.value += (x.innerHTML);
+                break;
             default:
                 result.value += (x.innerHTML);
+                console.log("tf")
                 break;
         }
     }
